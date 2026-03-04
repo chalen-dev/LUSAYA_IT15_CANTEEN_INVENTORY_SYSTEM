8292bd4ae5117406391a732122aa1ba3be145c7c
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2026-03-04 14:17:35 +0800

    chore: Remove default welcome view.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StockEntryController;
+use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+Route::resource('products', ProductController::class);
+Route::resource('suppliers', SupplierController::class);
+Route::resource('stock-entries', StockEntryController::class);
+
+
